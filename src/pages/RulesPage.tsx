e1eb19196a0be493a240d32d650779eb08c85165
banhpete <peter@a2xaccounting.com>
2026-06-06 08:33:38 -0400

    Embed yt vid

--- src/pages/RulesPage.tsx
+++ src/pages/RulesPage.tsx
@@ -5,6 +5,14 @@ export default function RulesPage() {
   return (
     <div className="rules-page">
       <img src={rulesImg} alt="Tournament rules" />
+      <div className="rules-video">
+        <iframe
+          src="https://www.youtube.com/embed/fTvPYdKZqO0"
+          title="Rules video"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
     </div>
   )
 }
